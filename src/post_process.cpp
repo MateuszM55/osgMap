41acb0d45a18d1aaf2ad43a923cd552c393fea83
JulianKonowalski <julian.konwalski@gmail.com>
2026-01-16 15:57:59 +0100

    customisable postfx pipeline

--- src/post_process.cpp
+++ src/post_process.cpp
@@ -20,54 +20,114 @@ static std::string s_shader_path = SHADER_PATH;
 // GOOD LUCK! // Thanks, I'll need it
 
 /**************************************************************************************************/
+/* MISC                                                                                           */
+/**************************************************************************************************/
 
-PostProcessor::PostProcessor(
-    osg::ref_ptr<osg::Group> parent
-) : m_render_camera(new osg::Camera),
-    m_render_plane(
-    osg::createTexturedQuadGeometry(
-        osg::Vec3(-1.0f, -1.0f, 0.0f),
-        osg::Vec3( 2.0f,  0.0f, 0.0f),
-        osg::Vec3( 0.0f,  2.0f, 0.0f))
-    ),
-    m_color_texture(new osg::Texture2D),
-    m_depth_texture(new osg::Texture2D)
+osg::ref_ptr<osg::Program> createProgram(const std::string& vert_filename,
+    const std::string& frag_filename)
 {
-    parent->addChild(m_render_camera);
-
-    osg::ref_ptr<osg::Geode> render_plane_geode = new osg::Geode;
-    render_plane_geode->addDrawable(m_render_plane.get());
-
     osg::ref_ptr<osg::Program> program = new osg::Program;
-    program->addShader(osg::Shader::readShaderFile(osg::Shader::VERTEX, s_shader_path + "/passthrough.vert"));
-    program->addShader(osg::Shader::readShaderFile(osg::Shader::FRAGMENT, s_shader_path + "/fxaa.frag"));
+    program->addShader(osg::Shader::readShaderFile(
+        osg::Shader::VERTEX, s_shader_path + vert_filename));
+    program->addShader(osg::Shader::readShaderFile(
+        osg::Shader::FRAGMENT, s_shader_path + frag_filename));
+    return program;
+}
+
+/**************************************************************************************************/
+
+osg::ref_ptr<osg::Geode>
+createRenderPlane(osg::ref_ptr<osg::Program> program,
+                  osg::ref_ptr<osg::Texture2D> color_texture,
+                  osg::ref_ptr<osg::Texture2D> depth_texture)
+{
+    osg::ref_ptr<osg::Geode> render_plane_geode = new osg::Geode;
+    render_plane_geode->addDrawable(osg::createTexturedQuadGeometry(
+        osg::Vec3(-1.0f, -1.0f, 0.0f), osg::Vec3(2.0f, 0.0f, 0.0f),
+        osg::Vec3(0.0f, 2.0f, 0.0f)));
 
     osg::StateSet* state_set = render_plane_geode->getOrCreateStateSet();
-    state_set->setAttributeAndModes(program.get());
-
     state_set->setMode(GL_DEPTH_TEST, osg::StateAttribute::OFF);
     state_set->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
-
-    state_set->setTextureAttributeAndModes(0, m_color_texture.get());
-    state_set->setTextureAttributeAndModes(1, m_depth_texture.get());
+    state_set->setAttributeAndModes(program.get());
+    state_set->setTextureAttributeAndModes(0, color_texture.get());
+    state_set->setTextureAttributeAndModes(1, depth_texture.get());
     state_set->addUniform(new osg::Uniform("color_texture", 0));
     state_set->addUniform(new osg::Uniform("depth_texture", 1));
 
-    m_render_camera->setRenderOrder(osg::Camera::POST_RENDER);
-    m_render_camera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
-    m_render_camera->setViewMatrix(osg::Matrix::identity());
-    m_render_camera->setProjectionMatrix(osg::Matrix::ortho2D(-1, 1, -1, 1));
-    m_render_camera->setClearMask(0);
-    m_render_camera->addChild(render_plane_geode.get());
+    return render_plane_geode;
+}
 
-    m_color_texture->setInternalFormat(GL_RGBA);
-    m_color_texture->setFilter(osg::Texture::MIN_FILTER, osg::Texture::NEAREST);
-    m_color_texture->setFilter(osg::Texture::MAG_FILTER, osg::Texture::NEAREST);
+/**************************************************************************************************/
+/* LAYER                                                                                          */
+/**************************************************************************************************/
 
-    m_depth_texture->setInternalFormat(GL_DEPTH_COMPONENT24);
-    m_depth_texture->setSourceFormat(GL_DEPTH_COMPONENT);
+Layer::Layer(osg::ref_ptr<osg::Texture2D> in_color_texture,
+             osg::ref_ptr<osg::Texture2D> out_color_texture,
+             osg::ref_ptr<osg::Texture2D> depth_texture,
+             osg::ref_ptr<osg::Group> parent, int render_order,
+             const std::string& vert_filename, const std::string& frag_filename)
+    : m_camera(new osg::Camera)
+{
+    osg::ref_ptr<osg::Program> program = createProgram(vert_filename, frag_filename);
+    osg::ref_ptr<osg::Geode> render_plane_geode = createRenderPlane(program, in_color_texture, depth_texture);
+
+    m_camera->setRenderOrder(osg::Camera::PRE_RENDER, render_order);
+    m_camera->setRenderTargetImplementation(osg::Camera::FRAME_BUFFER_OBJECT);
+    m_camera->attach(osg::Camera::COLOR_BUFFER0, out_color_texture.get());
+    m_camera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
+    m_camera->setViewMatrix(osg::Matrix::identity());
+    m_camera->setProjectionMatrix(osg::Matrix::ortho2D(-1, 1, -1, 1));
+    m_camera->setClearMask(0);
+    m_camera->addChild(render_plane_geode.get());
+
+    parent->addChild(m_camera);
+}
+
+/**************************************************************************************************/
+
+Layer::~Layer(void) = default;
+
+/**************************************************************************************************/
+/* POST PROCESSOR                                                                                 */
+/**************************************************************************************************/
+
+PostProcessor::PostProcessor(osg::ref_ptr<osg::Group> parent)
+    : m_parent(parent), m_camera(new osg::Camera),
+      m_frame_texture(new osg::Texture2D), m_depth_texture(new osg::Texture2D)
+{
+    for (int i = 0; i < 2; ++i)
+    {
+        m_color_textures[i] = new osg::Texture2D;
+        m_color_textures[i]->setInternalFormat(GL_RGBA);
+        m_color_textures[i]->setFilter(osg::Texture::MIN_FILTER,
+                                      osg::Texture::NEAREST);
+        m_color_textures[i]->setFilter(osg::Texture::MAG_FILTER,
+                                      osg::Texture::NEAREST);
+    }
+
+    m_frame_texture->setInternalFormat(GL_RGBA);
+    m_frame_texture->setFilter(osg::Texture::MIN_FILTER, osg::Texture::NEAREST);
+    m_frame_texture->setFilter(osg::Texture::MAG_FILTER, osg::Texture::NEAREST);
+
     m_depth_texture->setSourceType(GL_FLOAT);
+    m_depth_texture->setSourceFormat(GL_DEPTH_COMPONENT);
+    m_depth_texture->setInternalFormat(GL_DEPTH_COMPONENT24);
     m_depth_texture->setShadowComparison(false);
+
+    osg::ref_ptr<osg::Program> program =
+        createProgram("passthrough.vert", "passthrough.frag");
+    m_render_plane =
+        createRenderPlane(program, m_color_textures[0], m_depth_texture);
+
+    m_camera->setRenderOrder(osg::Camera::POST_RENDER);
+    m_camera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
+    m_camera->setViewMatrix(osg::Matrix::identity());
+    m_camera->setProjectionMatrix(osg::Matrix::ortho2D(-1, 1, -1, 1));
+    m_camera->setClearMask(0);
+    m_camera->addChild(m_render_plane.get());
+
+    parent->addChild(m_camera);
 }
 
 /**************************************************************************************************/
@@ -82,23 +142,34 @@ PostProcessor::~PostProcessor(void)
 
 /**************************************************************************************************/
 
-void PostProcessor::attachInputCamera(osg::ref_ptr<osg::Camera> camera) 
+void PostProcessor::resize(int width, int height) {
+    m_frame_texture->setTextureSize(width, height);
+    m_depth_texture->setTextureSize(width, height);
+    for (int i = 0; i < 2; ++i)
+    {
+        m_color_textures[i]->setTextureSize(width, height);
+    }
+
+    m_camera->setViewport(0, 0, width, height);
+    for (auto* layer : m_layers)
+    {
+        layer->setViewport(0, 0, width, height);
+    }
+}
+
+/**************************************************************************************************/
+
+void PostProcessor::attachInputCamera(osg::ref_ptr<osg::Camera> camera)
 {
-    camera->setRenderOrder(osg::Camera::PRE_RENDER);
+    camera->setRenderOrder(osg::Camera::PRE_RENDER, 0);
     camera->setRenderTargetImplementation(osg::Camera::FRAME_BUFFER_OBJECT);
-    camera->attach(osg::Camera::COLOR_BUFFER0, m_color_texture.get());
-    camera->attach(osg::Camera::DEPTH_BUFFER, m_depth_texture.get());
-
-    camera->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     camera->setClearColor(osg::Vec4(0.0f, 0.0f, 0.0f, 1.0f));
+    camera->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    camera->attach(osg::Camera::DEPTH_BUFFER, m_depth_texture.get());
+    camera->attach(osg::Camera::COLOR_BUFFER0, m_frame_texture.get());
 
-    m_color_texture->setTextureWidth(camera->getViewport()->width());
-    m_depth_texture->setTextureWidth(camera->getViewport()->width());
-    m_color_texture->setTextureHeight(camera->getViewport()->height());
-    m_depth_texture->setTextureHeight(camera->getViewport()->height());
-
-    m_render_camera->setViewport(0, 0, camera->getViewport()->width(),
-                                 camera->getViewport()->height());
+    this->resize(camera->getViewport()->width(),
+                 camera->getViewport()->height());
 }
 
 /**************************************************************************************************/
--- src/post_process.h
+++ src/post_process.h
@@ -18,82 +18,122 @@ namespace osgMap::postfx {
 
 class Layer {
 public:
-	
-	Layer(void) = default;
-    virtual ~Layer(void) = default;
-	
-private:
+    Layer(osg::ref_ptr<osg::Texture2D> in_color_texture,
+          osg::ref_ptr<osg::Texture2D> out_color_texture,
+          osg::ref_ptr<osg::Texture2D> depth_texture,
+          osg::ref_ptr<osg::Group> parent, int render_order,
+          const std::string& vert_filename, const std::string& frag_filename);
+    virtual ~Layer(void) = 0;
 
+    inline void setViewport(int x, int y, int width, int height)
+    {
+        m_camera->setViewport(x, y, width, height);
+    }
+
+private:
+    osg::ref_ptr<osg::Camera> m_camera;
 };
 
 /**************************************************************************************************/
 
 class DOF : public Layer {
-
+public:
+    DOF(osg::ref_ptr<osg::Texture2D> in_color_texture,
+        osg::ref_ptr<osg::Texture2D> out_color_texture,
+        osg::ref_ptr<osg::Texture2D> depth_texture,
+        osg::ref_ptr<osg::Group> parent, int render_order)
+        : Layer(in_color_texture, out_color_texture, depth_texture, parent,
+                render_order, "passthrough.vert", "dof.frag")
+    {}
+    ~DOF(void) override {}
 };
 
 /**************************************************************************************************/
 
 class Bloom : public Layer {
-
+public:
+    Bloom(osg::ref_ptr<osg::Texture2D> in_color_texture,
+          osg::ref_ptr<osg::Texture2D> out_color_texture,
+          osg::ref_ptr<osg::Texture2D> depth_texture,
+          osg::ref_ptr<osg::Group> parent, int render_order)
+        : Layer(in_color_texture, out_color_texture, depth_texture, parent,
+                render_order, "passthrough.vert", "bloom.frag")
+    {}
+    ~Bloom(void) override {}
 };
 
 /**************************************************************************************************/
 
 class FXAA : public Layer {
-
+public:
+    FXAA(osg::ref_ptr<osg::Texture2D> in_color_texture,
+         osg::ref_ptr<osg::Texture2D> out_color_texture,
+         osg::ref_ptr<osg::Texture2D> depth_texture,
+         osg::ref_ptr<osg::Group> parent, int render_order)
+        : Layer(in_color_texture, out_color_texture, depth_texture, parent,
+                render_order, "passthrough.vert", "fxaa.frag")
+    {}
+    ~FXAA(void) override {}
 };
 
 /**************************************************************************************************/
 
 class PostProcessor {
 public:
+    PostProcessor(osg::ref_ptr<osg::Group> parent);
+    ~PostProcessor(void);
 
-	PostProcessor(osg::ref_ptr<osg::Group> parent);
-	~PostProcessor(void);
-
-	template <typename T> void pushLayer(void);
+    template <typename T> void pushLayer(void);
     template <typename T> T* getLayer(void);
 
-	void attachInputCamera(osg::ref_ptr<osg::Camera> camera);
+    void resize(int width, int height);
+    void attachInputCamera(osg::ref_ptr<osg::Camera> camera);
 
 private:
-
-    osg::ref_ptr<osg::Camera> m_render_camera;
-	osg::ref_ptr<osg::Geometry> m_render_plane;
-    osg::ref_ptr<osg::Texture2D> m_color_texture;
+    osg::ref_ptr<osg::Group> m_parent;
+    osg::ref_ptr<osg::Camera> m_camera;
+    osg::ref_ptr<osg::Geode> m_render_plane;
+    osg::ref_ptr<osg::Texture2D> m_color_textures[2];
+    osg::ref_ptr<osg::Texture2D> m_frame_texture;
     osg::ref_ptr<osg::Texture2D> m_depth_texture;
-	
-	std::vector<Layer*> m_layers;
 
+    std::vector<Layer*> m_layers;
 };
 
 /**************************************************************************************************/
 
-template <typename T> 
-void PostProcessor::pushLayer(void) {
-	static_assert(std::is_base_of<Layer, T>::value,
-		"T must be derived from osgMap::postfx::Layer");
+template <typename T> void PostProcessor::pushLayer(void)
+{
+    static_assert(std::is_base_of<Layer, T>::value,
+                  "T must be derived from osgMap::postfx::Layer");
 
-    m_layers.push_back(new T);
+    int in_idx = m_layers.size() & 0x1;
+    int out_idx = (m_layers.size() + 1) & 0x1;
+
+    m_layers.push_back(
+        new T(!m_layers.size() ? m_frame_texture : m_color_textures[in_idx],
+              m_color_textures[out_idx], m_depth_texture, m_parent,
+              m_layers.size() + 1));
+    m_render_plane->getOrCreateStateSet()->setTextureAttributeAndModes(
+        0, m_color_textures[out_idx].get());
 }
 
 /**************************************************************************************************/
 
-template <typename T>
-T* PostProcessor::getLayer(void) {
-	static_assert(std::is_base_of<Layer, T>::value,
-		"T must be derived from osgMap::postfx::Layer");
+template <typename T> T* PostProcessor::getLayer(void)
+{
+    static_assert(std::is_base_of<Layer, T>::value,
+                  "T must be derived from osgMap::postfx::Layer");
 
-	for (auto* layer : m_layers)
-	{	
-		if (auto* casted_layer = dynamic_cast<T*>(layer))
-		{
-			return casted_layer;
-		}
-	}
+    for (auto* layer : m_layers)
+    {
+        if (auto* casted_layer = dynamic_cast<T*>(layer))
+        {
+            return casted_layer;
+        }
+    }
 
-	return nullptr;
+    return nullptr;
 }
 
 /**************************************************************************************************/
